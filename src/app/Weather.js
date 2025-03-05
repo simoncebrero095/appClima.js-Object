989bffad10f3e369bc8b05ec5a399f8cf2c9c24e
Simón Cebrero <simoncebrero95@gmail.com>
2025-03-04 22:42:00 -0300

    Definir rutas y funcionalidad a la funciones

--- src/app/Weather.js
+++ src/app/Weather.js
@@ -1,3 +1,25 @@
+export class Weather {
+
+    constructor(city, countryCode){
+        this.apiKey = '8a0e49f7d8b142dafb9a40270ac1a879'
+        this.city = city;
+        this.countryCode = countryCode;
+
+    }
+
+    async getWeather(){
+        const URI  = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}&units=metric`;
+        const response = await fetch(URI);
+        const data = await response.json()
+        return data;
+    }
+
+changeLocation(city , countryCode){
+        this.city = city;
+        this.countryCode = countryCode; 
+}
+
+}
 
 
 
@@ -6,4 +28,4 @@
 
 
 
-
+ 
--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,26 @@
+const {Weather} = require ('./Weather');
+const {UI} = require ('./UI'); 
+
+const ui = new UI()
+const weather = new Weather('London','uk');
 require('./index.css');
 
-function fetchWeather (){
-
+async function fetchWeather (){
+    const data = await weather.getWeather();
+    console.log(data)
+    ui.render(data)
+    
 }
-document.addEventListener('DOMContentLoaded',)
+document.getElementById('w-change-btn').addEventListener('click',(e) =>{
+    const city = document.getElementById('city').value;
+    const countryCode = document.getElementById('countryCode').value;
+    weather.changeLocation(city, countryCode);
+    fetchWeather();
+    e.preventDefault();
+});
+
+document.addEventListener('DOMContentLoaded',fetchWeather);
+    
 
 
 
